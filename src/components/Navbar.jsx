64545ee52090bb80930aab6b9e83690b29d5ff70
Deniz Cilingir <deniz.cilingir@gmx.de>
2024-02-08 17:23:00 +0100

    Fix Styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import styles from "./Navbar.module.css";
+
 function Navbar() {
-    return (
-        <nav className="navbar">
-        <img src="/src/assets/Logo.png"/>
-        <h1>Movie Explorer</h1>
-        </nav>
-    )
+  return (
+    <nav className={styles.navbar}>
+      <img src="/src/assets/Logo.png" />
+      <h1>Movie Explorer</h1>
+    </nav>
+  );
 }
 
 export default Navbar;
